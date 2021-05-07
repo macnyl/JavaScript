6dd82aeea83d437434faabaf9dafb8c90e2b9c2f
Macnyl <grou@protonmail.com>
2021-05-07 16:02:55 +0300

    homework

---
+++ index.js
@@ -0,0 +1,136 @@
+const a = 2;
+const b = 3;
+console.log('a * b :>> ', a * b);
+
+const c = 10;
+const d = 2;
+console.log('c / d :>> ', c / d);
+
+const e = 7;
+const f = 3;
+console.log('e + f :>> ', e + f);
+
+
+let eleven = 11;
+eleven = true;
+console.log('11:>> ', eleven);
+
+let hundred = 100;
+hundred = true;
+console.log('100 :>> ', hundred);
+
+let javaScript = "JavaScript";
+javaScript = true;
+console.log('JavaScript:>> ', javaScript);
+ 
+let num = 1;
+num = num + 11;
+num = num - 11;
+num = num * 11;
+num = num / 11;
+num = num + 1;
+num = num - 1;
+console.log('num = num += 11 :>> ', num = num += 11);
+console.log('num = num -= 11 :>> ', num = num -= 11);
+console.log('num = num *= 11 :>> ', num = num *= 11);
+console.log('num = num /= 11 :>> ', num = num / 11);
+console.log('num = num ++ 11 :>> ', num =++ num + 11); 
+console.log('num = num -- 11 :>> ', num =-- num - 11);
+
+// const number = prompt("Enter your number");
+// alert(number * number);
+
+// const number = prompt("Enter your number");
+// const number2 = prompt("Enter your another number");
+// alert(number * number2 / 2);
+
+// const number = prompt("Enter minutes");
+// const text = "Answer: ";
+// const answer = 60;
+// alert(text + number * answer);
+
+// const userName = prompt("Enter your name");
+// const greeting = "Hello ";
+// const symbol = "!";
+// alert(greeting + userName + symbol);
+
+// const theRightNumber = prompt("Enter the right number:");
+// if (theRightNumber == 10){
+//     alert("Right!");
+// } else {
+//  alert("Wrong!");
+// }
+
+// const newValue = 7;
+// let value;
+// do {
+//   value = Number(prompt("Enter value:"));
+// } while ((value !== newValue))
+// alert('You are lucky! \u{1F600}');
+let i = 11;
+let result1 = 0;
+do {                                    
+  alert('i :>> ', i);
+  result1 += i;
+  i++;
+} while (i <= 10)
+alert('result :>> ', result1);
+
+// const number = prompt("Enter purchase amount:");
+// const discount = 3 % (number);
+// const textDiscount = "Your discounted amount: ";
+// alert(textDiscount + (number - discount));
+
+// const valueDiscount = prompt('Enter the amount:')
+// if(valueDiscount >500 && valueDiscount <=800){
+//     let discount = valueDiscount / 100 * 97;
+//     alert('Purchase amount:' + discount)
+// }else{
+//     if(valueDiscount > 800){
+//         let discountNumberTwo = valueDiscount / 100 * 95;
+//         alert('Purchase amount')
+//     }
+// }
+
+// const print = 0;
+// let score = 25;
+// while (score >= print) {
+// console.log(score);
+// score--;
+// }
+
+// for (let i = 25; i >= 0; i--){
+//     if (i % 2 == 1){
+//         console.log('i :>> ', i);
+//     }
+// }
+
+// const print = 10;
+// let score = 50;
+// let amount = 5;
+// while (score >= print) {
+// console.log(score);
+// score + amount;
+// }
+
+// for (let i = 10; i <= 50; i++){
+//     if (i % 5 == 1 - 1){
+//         console.log('i :>> ', i);
+//     }
+// }
+
+// const findNumber = 77;
+// let value;
+// let request = "Congratulations! ";
+// do{
+//     value = Number(prompt("Enter from 1 to 100:"))
+// } while (value !== findNumber)
+// alert(request)
+
+// const wantNumber = 6;
+// let value;
+// do {
+//   value = Number(prompt('Solve the task 2 + 2 * 2 = '));
+// } while (value !== wantNumber)
+//alert('Right!')
+// console.log('Right!');
